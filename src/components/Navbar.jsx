5d85985b51e21bf51ca8e1f7e0b4074ad8312bcb
Bryan Sanudo Garcia <bryansanudo95@gmail.com>
2023-02-15 16:03:25 -0500

    navigate

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,21 +9,21 @@ const Navbar = ({ isMenuShown, setIsMenuShown }) => {
   const links = [
     {
       id: 1,
-      link: "https://sarazapata.vercel.app/",
+      link: "https://sara-marcas.vercel.app/",
       src: logoSara,
       brand:
         "rounded-full object-cover w-20 h-20 md:w-40 md:h-40 shadow-[#6057ca] hover:shadow-[#6057ca] duration-300 cursor-pointer hover:shadow-xl shadow-md",
     },
     {
       id: 2,
-      link: "",
+      link: "https://sara-madwax.vercel.app/",
       src: logoMdwx,
       brand:
         "rounded-full object-cover w-20 h-20 md:w-40 md:h-40 shadow-[#29fd04] hover:shadow-[#29fd04]   duration-300 cursor-pointer hover:shadow-xl shadow-md",
     },
     {
       id: 3,
-      link: "",
+      link: "#",
       src: logoJmarioCouture,
       brand:
         "rounded-full object-cover w-20 h-20 md:w-40 md:h-40 shadow-[#fff] hover:shadow-[#fff] duration-300  cursor-pointer hover:shadow-xl shadow-md",
